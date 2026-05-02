31986e2e2df222f64a2689724ba669038165c06b
UCB10112 <dhaydee693@gmail.com>
2026-05-02 12:47:53 -0500

    Clase Alumno Controlador

---
+++ src/institutoinnova/AlumnoControlador.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package institutoinnova;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author LCB10112
+ */
+public class AlumnoControlador {
+    ArrayList<Alumno> listaAlumnos = new ArrayList<>();
+ 
+    void agregarAlumno(Alumno a) {
+        listaAlumnos.add(a);
+    }
+ 
+    void listarAlumnos() {
+        System.out.println("LISTA DE ALUMNOS: ");
+        if (listaAlumnos.isEmpty()) {
+            System.out.println("  (No hay alumnos registrados)");
+        } else {
+            for (Alumno a : listaAlumnos) {
+                a.verDatos();
+            }
+        }
+    }
+}
